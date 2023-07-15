d2cb5db6a0d7893e4e26716e5c09b2f3158a8e90
Anatoliy Sharov <sharov.012@gmail.com>
2023-07-15 13:17:50 +0300

    исправил адрес запроса к сайту

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const app = express();
 mongoose.connect('mongodb://127.0.0.1:27017/mestodatabase');
 app.use(express.json());
 app.use(helmet());
-app.use(cors({ credentials: true, origin: 'http://meste4ko.nomoredomains.work' }));
+app.use(cors({ credentials: true, origin: 'https://meste4ko.nomoredomains.work' }));
 app.options('*', cors());
 app.use(cookieParser());
 app.use(requestLogger);
